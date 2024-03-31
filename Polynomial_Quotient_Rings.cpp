9432b7886d6ab12842faf7818b3ad40cfc2cd93b
Ryan Patrick <rmoneymail5@gmail.com>
2024-03-30 19:56:14 -0700

    Initial commit.

---
+++ Polynomial_Quotient_Rings.cpp
@@ -0,0 +1,24 @@
+// File: Polynomial_Quotient_Rings.cpp
+// Author: Ryan P.
+// Timestamp: <30-Mar-2024> 19:43:31 PDT
+// Purpose: Main driver for a program which prints arithmetic tables for polynomial quotient rings.
+
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string ringOfCoefficients;
+    printf("Enter the ring of coefficients of the polynomials:");
+    cin >> ringOfCoefficients;
+
+    string modulus;
+    printf("Enter a polynomial modulus:");
+    cin >> modulus;
+
+    return 0;
+}
